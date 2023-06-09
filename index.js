132ab698ad74425bbd3433e1746be06d14c444d8
Amit Sarker <amitsarkerstock@gmail.com>
2023-06-09 08:27:01 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -2,15 +2,43 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 3030;
 require("dotenv").config();
 
-app.use(express.json());
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
 
-app.get("/", (req, res) => {
-  res.send("Guru is running...");
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASS}@cluster0.pq4nrld.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    app.get("/", (req, res) => {
+      res.send("Guru is running...");
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Guru is rummimg on port: ${port}`);
